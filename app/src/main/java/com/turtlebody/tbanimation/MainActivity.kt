dfdc54fcd171392d9d0722630ec99a58aa84a9fd
niraj prakash <nirajprakash13@gmail.com>
2019-01-03 14:42:58 +0530

    added testing

--- app/src/main/java/com/turtlebody/tbanimation/MainActivity.kt
+++ app/src/main/java/com/turtlebody/tbanimation/MainActivity.kt
@@ -2,11 +2,15 @@ package com.turtlebody.tbanimation
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.turtlebody.tbanimation.ui.components.anim1.Anim1Activity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        test_btn_anim_circle.setOnClickListener { startActivity<Anim1Activity>()  }
     }
 }
