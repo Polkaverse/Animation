7560dbfabfaa3c3de5ba150da5244219c7cb9bb8
pankajchaudhary5 <pankajchaudhary172@gmail.com>
2019-01-10 19:41:29 +0530

    Circle aniamtion by ObjectAnimator

--- app/src/main/java/com/turtlebody/tbanimation/ui/components/anim1/CustomView.java
+++ app/src/main/java/com/turtlebody/tbanimation/ui/components/anim1/CustomView.java
@@ -1,20 +1,32 @@
 package com.turtlebody.tbanimation.ui.components.anim1;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.widget.Toast.LENGTH_LONG;
+
 
 public class CustomView extends View {
+
+    CustomView customView;
+    public int baseRadius=20;
     Paint paint;
     int radius = 0;
+    float min=100f , max=200f;
 
     public CustomView(Context context) {
         super(context);
         paint = new Paint();
         paint.setColor(Color.RED);
-
     }
 
     public void updateView(int radius) {
@@ -28,4 +40,36 @@ public class CustomView extends View {
         canvas.drawCircle(getWidth() / 2, getHeight() / 2, radius, paint);
     }
 
+
+    public void startAnimate(){
+        ObjectAnimator first=ObjectAnimator.ofFloat(this,"circleRadius",min,max);
+        first.setDuration(1000);
+        first.start();
+
+        ObjectAnimator second=ObjectAnimator.ofFloat(this,"circleRadius",max,min);
+        second.setDuration(1000);
+        second.start();
+
+        AnimatorSet bouncer = new AnimatorSet();
+        bouncer.play(first).after(second);
+
+        AnimatorSet bouncer1 = new AnimatorSet();
+        bouncer1.play(first).after(second);
+
+        AnimatorSet bouncer2 = new AnimatorSet();
+        bouncer2.play(bouncer).after(bouncer1);
+
+        AnimatorSet bouncer3 = new AnimatorSet();
+        bouncer3.play(bouncer).after(bouncer2);
+        bouncer3.start();
+        }
+
+    float getCircleRadius(){
+      return 0f;
+    }
+
+    void setCircleRadius(float a){
+        updateView(Math.round(a));
+
+    }
 }
--- app/src/main/java/com/turtlebody/tbanimation/ui/components/anim2/Anim2Activity.java
+++ app/src/main/java/com/turtlebody/tbanimation/ui/components/anim2/Anim2Activity.java
@@ -1,10 +1,16 @@
 package com.turtlebody.tbanimation.ui.components.anim2;
 
+import android.animation.ObjectAnimator;
+import android.graphics.Canvas;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.PathInterpolator;
 import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import com.turtlebody.tbanimation.R;
 import com.turtlebody.tbanimation.ui.components.anim1.CustomView;
@@ -20,6 +26,5 @@ public class Anim2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_anim2);
 
     }
-
 
 }
