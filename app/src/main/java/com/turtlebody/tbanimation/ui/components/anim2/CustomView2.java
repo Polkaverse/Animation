af291458584528bb0ca39da6b1f301f0f2b47e6e
pankajchaudhary5 <pankajchaudhary172@gmail.com>
2019-01-16 23:56:04 +0530

    Final Circle And Square Animation

--- app/src/main/java/com/turtlebody/tbanimation/ui/components/anim2/CustomView2.java
+++ app/src/main/java/com/turtlebody/tbanimation/ui/components/anim2/CustomView2.java
@@ -22,7 +22,7 @@ public class CustomView2 extends View{
     //Rectangle
     private Rect mRect;
 
-    int mRectSize = 350 , p  ;     // p is the Flag Variable
+    int mRectSize = 350 , p;     // p is the Flag Variable
     final static float RECT_MIN_SIDE =350f , RECT_MAX_SIZE =410f;
 
     //Circle
@@ -78,53 +78,32 @@ public class CustomView2 extends View{
 
     // Animate Function
     public void startAnimate() {
-        // Used ObjectAnimator for Square
-        ObjectAnimator first = ObjectAnimator.ofFloat(this, "squareSide", RECT_MIN_SIDE, RECT_MAX_SIZE);
-        first.setRepeatCount(ObjectAnimator.INFINITE);
-        first.setRepeatMode(ObjectAnimator.REVERSE);
 
-        // Used ObjectAnimator for Circle
-        ObjectAnimator second = ObjectAnimator.ofFloat(this, "circleX", CIRCLE_MIN_X, CIRCLE_MAX_X);
-        second.setRepeatCount(ObjectAnimator.INFINITE);
-        second.setRepeatMode(ObjectAnimator.REVERSE);
-
-        // Animator set to animate two object parallely
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(first,second);
-        animatorSet.setDuration(1500);
-        animatorSet.start();
+        ObjectAnimator Animat = ObjectAnimator.ofFloat(this, "viewAnimation", RECT_MIN_SIDE, RECT_MAX_SIZE);
+        Animat.setDuration(1000);
+        Animat.setRepeatCount(ObjectAnimator.INFINITE);
+        Animat.setRepeatMode(ObjectAnimator.REVERSE);
+        Animat.start();
 
     }
 
-    //Circle Getter, Setter and Update Function
-    float getCircleX(){
+    float getViewAnimation(){
         return 0f;
     }
 
-    void setCircleX(float a ){
-        updateCircleView(Math.round(a));
-    }
-    public void updateCircleView(int side) {
-        mCircleX = side;
-        invalidate();
+    void setViewAnimation(float a ){
+        updateAnimationView(Math.round(a));
     }
 
-    //Square Getter, Setter and Update Function
-    float getSquareSide(){
-        return 0f;
-    }
-
-    void setSquareSide(float a){
-        updateSquareView(Math.round(a));
-    }
-
-    public void updateSquareView(int side) {
+    private void updateAnimationView(int side) {
         p=side - mRectSize;
         mRectSize =side ;
         mRect.left = mRect.left -p;
         mRect.top = mRect.top -p ;
         mRect.right = mRectSize;
         mRect.bottom = mRectSize;
+
+        mCircleX = (int) ((side-RECT_MIN_SIDE)*(CIRCLE_MAX_X-CIRCLE_MIN_X)/(RECT_MAX_SIZE-RECT_MIN_SIDE)+CIRCLE_MIN_X);
         invalidate();
     }
 
