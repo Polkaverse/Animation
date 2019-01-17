aaaf8d2fbe7cfd1fd4a06e6bdfc7308555d124c2
pankajchaudhary5 <pankajchaudhary172@gmail.com>
2019-01-18 02:03:49 +0530

    Start 3rd assignment

--- app/src/main/java/com/turtlebody/tbanimation/MainActivity.kt
+++ app/src/main/java/com/turtlebody/tbanimation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.turtlebody.tbanimation.ui.components.anim1.Anim1Activity
 import com.turtlebody.tbanimation.ui.components.anim2.Anim2Activity
+import com.turtlebody.tbanimation.ui.components.anim3.Anim3Activity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
@@ -14,5 +15,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         test_btn_anim_circle.setOnClickListener { startActivity<Anim1Activity>()  }
         test_btn_anim_circle2.setOnClickListener{startActivity<Anim2Activity>()}
+        test_btn_anim_circle3.setOnClickListener{startActivity<Anim3Activity>()}
     }
 }
